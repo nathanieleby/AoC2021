b1451c51ea2d3dbd37b385eb42690b54f130422f
Nathaniel Moore <nathaniel.moore@aseifl.com>
2021-12-01 17:05:27 -0600

    Finished up the input reading function. Solved day1 part1 in main.

---
+++ dec1/AoC1/GetInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace AoC1
+{
+    class GetInput
+    {
+        public static string[] ReadInputFile()
+        {
+            Console.WriteLine("This is the input function");
+            //FileStream inputFile = File.OpenRead("input.txt");
+
+            string[] contents = File.ReadAllLines("input.txt");
+
+            return contents;
+        }
+    }
+}
---
+++ dec1/AoC1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+
+namespace AoC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer   = 0;
+            int current  = 0;
+            int previous = 0;
+
+            Console.WriteLine("Hello World!");
+            string[] inFile = GetInput.ReadInputFile();
+
+            Console.WriteLine(inFile.Length + "Waddup Bitch");
+
+            for (int i = 1; i<inFile.Length; i++ )
+            {
+                int.TryParse(inFile[i], out current);
+                int.TryParse(inFile[i-1], out previous);
+                if (current > previous)
+                {
+                    answer += 1;
+                }
+            }
+
+            Console.WriteLine(answer);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
