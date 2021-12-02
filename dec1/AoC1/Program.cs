95b0de177a13ab0eb7e68f954ed5e5e1b1144be8
Nathaniel Moore <nathaniel.moore@aseifl.com>
2021-12-02 08:53:22 -0600

    Solved day 1

--- dec1/AoC1/Program.cs
+++ dec1/AoC1/Program.cs
@@ -1,26 +1,23 @@
 ﻿using System;
+using System.Linq;
 
 
 
 namespace AoC1
 {
+
     class Program
     {
-        static void Main(string[] args)
+        static void Day1Part1(string[] inFile)
         {
-            int answer   = 0;
-            int current  = 0;
-            int previous = 0;
+            int answer = 0;
+            int current;
+            int previous;
 
-            Console.WriteLine("Hello World!");
-            string[] inFile = GetInput.ReadInputFile();
-
-            Console.WriteLine(inFile.Length + "Waddup Bitch");
-
-            for (int i = 1; i<inFile.Length; i++ )
+            for (int i = 1; i < inFile.Length; i++)
             {
                 int.TryParse(inFile[i], out current);
-                int.TryParse(inFile[i-1], out previous);
+                int.TryParse(inFile[i - 1], out previous);
                 if (current > previous)
                 {
                     answer += 1;
@@ -28,8 +25,51 @@ namespace AoC1
             }
 
             Console.WriteLine(answer);
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
         }
+
+        static void Day1Part2(string[] inFile)
+        {
+            int answer = 0;
+            int[] first  = new int[3];
+            int[] second = new int[3];
+
+            int previous;
+            int current;
+
+            for (int i = 3; i < inFile.Length; i++)
+            {
+                int.TryParse(inFile[i - 1], out first[0]);
+                int.TryParse(inFile[i - 2], out first[1]);
+                int.TryParse(inFile[i - 3], out first[2]);
+
+                int.TryParse(inFile[i],     out second[0]);
+                int.TryParse(inFile[i - 1], out second[1]);
+                int.TryParse(inFile[i - 2], out second[2]);
+
+
+                if (first.Sum() < second.Sum())
+                {
+                    answer += 1;
+                }
+            }
+
+            Console.WriteLine(answer);
+        }
+        static void Main(string[] args)
+        {
+
+            string[] inFile = GetInput.ReadInputFile();
+
+            Console.WriteLine("Input File Lenght: " + inFile.Length );
+
+            Day1Part1( inFile);
+            Day1Part2( inFile);
+
+
+            //Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
+        }
+
+        
     }
 }
