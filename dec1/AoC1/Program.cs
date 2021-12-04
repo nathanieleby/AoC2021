f3713e51d084e6594955ac57d7edbae58b0e291a
ultimateeby@gmail.com <ultimateeby@gmail.com>
2021-12-03 21:57:55 -0600

    started day 3 part 1. Need to figure out how to bit flip epsilon

--- dec1/AoC1/Program.cs
+++ dec1/AoC1/Program.cs
@@ -11,11 +11,12 @@ namespace AoC
         static void Main(string[] args)
         {
 
-            string[] inFile = GetInput.ReadInputDay2();
+            string[] inFile = GetInput.ReadInputFile();
 
-            Day2.Part1( inFile );
-            Day2.Part2( inFile );
-
+            //Day2.Part1( inFile );
+            //Day2.Part2( inFile );
+            Day3.Part1( inFile );
+            Day3.Part2( inFile );
 
         }
 
---
+++ dec1/AoC1/day3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AoC
+{
+    class Day3
+    {
+        public static int Part1(string[] numbers)
+        {
+           
+            int i=0, j=0, gamma=0, epsilon=0, tally = 0;
+
+            for (i = numbers[j].Length-1 ; i >= 0 ; i--)
+            {
+                tally = 0;
+                for (j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[j][i] == '1')
+                        tally += 1;
+
+
+                }
+
+                if (tally > numbers.Length/2)
+                {
+                    epsilon |= ( 1 << i );
+                }
+            }
+
+
+
+            int result = epsilon * gamma;
+
+            Console.WriteLine(epsilon);
+            Console.WriteLine("Answer  1 = " + result );
+
+            return result;
+        }
+
+        public static int Part2(string[] numbers)
+        {
+
+
+            Console.WriteLine("Answer  2 = " );
+
+            return 0;
+        }
+    }
+}
