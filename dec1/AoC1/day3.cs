5a12b37a77ff0e0ad6e7c2096b2b4ae43dfbd02a
ultimateeby@gmail.com <ultimateeby@gmail.com>
2021-12-07 19:52:59 -0600

    Solved day3

--- dec1/AoC1/day3.cs
+++ dec1/AoC1/day3.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace AoC
 {
     class Day3
@@ -46,11 +47,12 @@ namespace AoC
             List<string> co2Code    = new List<string>(numbers);
 
             int i = 0, j = 0;
-            int gamma   = 0;
-            int epsilon = 0;         
+            uint oxygen   = 0;
+            uint co2 = 0;         
             int tally   = 0;
-            int result  = 0;
+            ulong result  = 0;
             char leastCommon = '0';
+            char mostCommon = '0';
 
             i = 0;
             while(oxygenCode.Count > 1 && i < oxygenCode[0].Length)
@@ -62,7 +64,7 @@ namespace AoC
                         tally += 1;
                 }
 
-                if (tally >= (oxygenCode.Count / 2) )
+                if (oxygenCode.Count - tally*2  <= 0  )
                 {
                     leastCommon = '0';
                 }
@@ -77,15 +79,60 @@ namespace AoC
                         oxygenCode.RemoveAt(j);
                 }
 
+
                 if (oxygenCode.Count == 1)
                     break;
                 
                 i++;
             }
 
-            result = epsilon * gamma;
 
-            return 0;
+
+            i = 0;
+            while (co2Code.Count > 1 && i < co2Code[0].Length)
+            {
+                tally = 0;
+                for (j = 0; j < co2Code.Count; j++)
+                {
+                    if (co2Code[j][i] == '0')
+                        tally += 1;
+                }
+
+                if (co2Code.Count - tally*2 < 0)
+                {
+                    mostCommon = '0';
+                }
+                else
+                {
+                    mostCommon = '1';
+                }
+
+                for (j = co2Code.Count - 1; j >= 0; j--)
+                {
+                    if (co2Code[j][i] == mostCommon)
+                        co2Code.RemoveAt(j);
+                }
+
+
+                if (co2Code.Count == 1)
+                    break;
+
+                i++;
+            }
+
+            //for (i = oxygenCode[0].Length - 1; i >= 0; i-- )
+            for (i = 0; i < oxygenCode[0].Length; i++)
+            {
+                if(oxygenCode[0][i] == '1')
+                    oxygen += (uint)Math.Pow(2, oxygenCode[0].Length - i - 1);
+
+                if (co2Code[0][i] == '1')
+                    co2 += (uint)Math.Pow(2, oxygenCode[0].Length - i - 1);
+            }
+
+            result = co2 * oxygen;
+
+            return (int)result;
         }
     }
 }
