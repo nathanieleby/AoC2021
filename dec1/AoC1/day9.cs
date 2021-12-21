993836ccc00e3afbee7c1a6c6bf895a1958fa2ee
Nathaniel Moore <nathaniel.moore@aseifl.com>
2021-12-21 17:06:07 -0600

    Started writing recursive function.

--- dec1/AoC1/day9.cs
+++ dec1/AoC1/day9.cs
@@ -9,14 +9,24 @@ namespace AoC
 
     class Day9
     {
-
+        class XYPair
+        {
+           public int x;
+           public int y;
+        }
+        
+        
         static int result = 0;
+        static bool[][] accessed;
+        private static  List<XYPair> xyPair = new List<XYPair>();
+        private static  int[][] grid;
+
         public static int Part1(string[] input)
         {
             result = 0;
             int i, j;
 
-            int[][] grid = parseInput(input);
+            grid = parseInput(input);
 
             for(i =0; i< input.Length;i++)
             {
@@ -28,13 +38,14 @@ namespace AoC
                     {
                         if (grid[i][j] < grid[i + 1][j] &&
                             grid[i][j] < grid[i][j + 1])
-                            result += grid[i][j] + 1;
+                            lowPointFound(i, j, grid);                                     
                     }
                     //upper right corner
                     else if (i == 0 && j == grid[i].Length - 1)
                     {
-                        if (grid[i][j] < grid[i + 1][j] && grid[i][j] < grid[i][j - 1])
-                            result += grid[i][j] + 1;
+                        if (grid[i][j] < grid[i + 1][j] && 
+                            grid[i][j] < grid[i][j - 1])
+                            lowPointFound(i, j, grid);
                     }
                     //top
                     else if (i == 0)
@@ -42,13 +53,14 @@ namespace AoC
                         if (grid[i][j] < grid[i + 1][j] &&
                             grid[i][j] < grid[i][j + 1] &&
                             grid[i][j] < grid[i][j - 1])
-                            result += grid[i][j] + 1;
+                            lowPointFound(i, j, grid);
                     }
                     //lower left corner
                     else if (i == grid.Length - 1 && j == 0)
                     {
-                        if (grid[i][j] < grid[i - 1][j] && grid[i][j] < grid[i][j + 1])
-                            result += grid[i][j] + 1;
+                        if (grid[i][j] < grid[i - 1][j] && 
+                            grid[i][j] < grid[i][j + 1])
+                            lowPointFound(i, j, grid);
                     }
                     //left
                     else if (j == 0)
@@ -56,14 +68,14 @@ namespace AoC
                         if (grid[i][j] < grid[i + 1][j] &&
                             grid[i][j] < grid[i - 1][j] &&
                             grid[i][j] < grid[i][j + 1])
-                            result += grid[i][j] + 1;
+                            lowPointFound(i, j, grid);
                     }
                     //lower right corner
                     else if (i == grid.Length - 1 && j == grid[i].Length - 1)
                     {
                         if (grid[i][j] < grid[i - 1][j] &&
                             grid[i][j] < grid[i][j - 1])
-                            result += grid[i][j] + 1;
+                            lowPointFound(i, j, grid);
                     }
                     
                     //right
@@ -72,7 +84,7 @@ namespace AoC
                         if (grid[i][j] < grid[i + 1][j] &&
                             grid[i][j] < grid[i - 1][j] &&
                             grid[i][j] < grid[i][j - 1])
-                            result += grid[i][j] + 1;
+                            lowPointFound(i, j, grid);
                     }
                     
                     //bottom
@@ -81,7 +93,7 @@ namespace AoC
                         if (grid[i][j] < grid[i - 1][j] &&
                             grid[i][j] < grid[i][j + 1] &&
                             grid[i][j] < grid[i][j - 1])
-                            result += grid[i][j] + 1;
+                            lowPointFound(i, j, grid);
                     }
                     //center of grid
                     else
@@ -90,7 +102,7 @@ namespace AoC
                             grid[i][j] < grid[i + 1][j] &&
                             grid[i][j] < grid[i][j + 1] &&
                             grid[i][j] < grid[i][j - 1])
-                            result += grid[i][j] + 1;
+                            lowPointFound(i, j, grid);
                     }
                 }
             }
@@ -107,20 +119,45 @@ namespace AoC
             return result;
         }
 
+        private static int checkGrid(int i, int j)
+        {
+            if (i < 0 ||
+                j < 0 ||
+                i > grid.Length -1 ||
+                i > grid[i].Length - 1
+                )
+                return 0;
 
+            return 1;
+        }
+
+        private static void lowPointFound(int i, int j, int[][] grid)
+        {
+            XYPair temp = new XYPair();
+            result += grid[i][j] + 1;
+
+            temp.x = i;
+            temp.y = j;
+
+            xyPair.Add(temp);
+        }
 
         private static int[][] parseInput(string[] input)
         {
             int i = 0, j;
             int[][] grid = new int[input.Length][ ];
 
+            accessed = new bool[input.Length][];
+
             for (i = 0; i < input.Length; i++)
             {
-                grid[i] = new int[input[i].Length];
+                grid[i]     = new int[input[i].Length];
+                accessed[i] = new bool[input[i].Length];
 
                 for (j = 0; j < input[i].Length; j++)
                 {
-                    grid[i][j] = int.Parse(input[i][j].ToString());
+                    grid[i][j]     = int.Parse(input[i][j].ToString());
+                    accessed[i][j] = false;
                 }
 
             }
