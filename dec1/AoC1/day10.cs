fa042d18a1cc10f5069040d5e2b91488a4c535aa
Nathaniel Moore <nathaniel.moore@aseifl.com>
2021-12-22 11:06:27 -0600

    Added day10 file

---
+++ dec1/AoC1/day10.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace AoC
+{
+
+    class Day10
+    {
+       
+        static int result = 0;
+
+
+        public static int Part1(string[] input)
+        {
+            result = 0;
+            
+
+            return result;
+        }
+
+        public static int Part2(string[] input)
+        {
+            result = 0;
+            int i, j;
+            
+
+            return result;
+        }
+
+        
+
+        private static void parseInput(string[] input)
+        {
+            int i = 0, j;
+
+
+            for (i = 0; i < input.Length; i++)
+            {
+
+
+                for (j = 0; j < input[i].Length; j++)
+                {
+
+                }
+
+            }
+            return;
+        }
+    }
+}
