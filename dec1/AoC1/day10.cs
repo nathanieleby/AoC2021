78254ec4c2d3ab2d9f1ff98b86075c7d5b472d41
Nathaniel Moore <nathaniel.moore@aseifl.com>
2021-12-29 17:55:45 -0600

    Finished day 10. Probably memory expensive but I don't care it took me so long.

--- dec1/AoC1/day10.cs
+++ dec1/AoC1/day10.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -20,6 +21,7 @@ namespace AoC
         static int result = 0;
 
         static int[] lookup = {3, 57, 1197, 25137};
+   
         static char closingParen   = ')';
         static char closingSquare  = ']';
         static char closingCurly   = '}';
@@ -36,83 +38,97 @@ namespace AoC
         static string angleChunk  = "<>";
 
         static errorLog errors = new errorLog();
+
+        private static List<long> scores = new List<long>();
+
+        private static List<string> inputList;
         public static int Part1(string[] input)
         {
             result = 0;
+
+            inputList = new List<string>(input);
 
             int i = 0, j = 0;
             int wasRemoved = 0;
             int errorFound = 0;
   
 
-            for (i = 0; i < input.Length; i++)
+            for (i = 0; i < inputList.Count; i++)
             {
                 errorFound = 0;
                 wasRemoved = 0;
-                for  (j = 0; j < input[i].Length; j++)
+                for  (j = 0; j < inputList[i].Length; j++)
                 {
                     errorFound = 0;
-                    if (isClosingBracket(input[i][j]))
+                    if (isClosingBracket(inputList[i][j]))
                     {
 
-                        if (input[i][j] == closingParen)
+                        if (inputList[i][j] == closingParen)
                         {
-                            if (input[i][j - 1] == openParen)
+                            if (inputList[i][j - 1] == openParen)
                             {
-                                input[i] = input[i].Remove(j - 1, 2);
+                                inputList[i] = inputList[i].Remove(j - 1, 2);
                                 j = 0;
                                 wasRemoved++;
                             }
-                            else if (!isClosingBracket(input[i][j - 1]))
+                            else if (!isClosingBracket(inputList[i][j - 1]))
                             {
-                                logErrors(input[i][j]);
+                                logErrors(inputList[i][j]);
                                 errorFound++;
+                                inputList.RemoveAt(i);
+                                i--;
                                 break;
                             }
 
                         }
-                        else if (input[i][j] == closingSquare)
+                        else if (inputList[i][j] == closingSquare)
                         {
-                            if (input[i][j - 1] == openSquare) 
-                            { 
-                                input[i] = input[i].Remove(j - 1, 2);
+                            if (inputList[i][j - 1] == openSquare) 
+                            {
+                                inputList[i] = inputList[i].Remove(j - 1, 2);
                                 j = 0;
                                 wasRemoved++;
                             }
-                            else if (!isClosingBracket(input[i][j - 1]))
+                            else if (!isClosingBracket(inputList[i][j - 1]))
                             {
-                                logErrors(input[i][j]);
+                                logErrors(inputList[i][j]);
                                 errorFound++;
+                                inputList.RemoveAt(i);
+                                i--;
                                 break;
                             }
                         }
-                        else if (input[i][j] == closingCurly)
+                        else if (inputList[i][j] == closingCurly)
                         {
-                            if (input[i][j - 1] == openCurly)
+                            if (inputList[i][j - 1] == openCurly)
                             {
-                                input[i] = input[i].Remove(j - 1, 2);
+                                inputList[i] = inputList[i].Remove(j - 1, 2);
                                 j = 0;
                                 wasRemoved++;
                             }
-                            else if (!isClosingBracket(input[i][j - 1]))
+                            else if (!isClosingBracket(inputList[i][j - 1]))
                             {
-                                logErrors(input[i][j]);
+                                logErrors(inputList[i][j]);
                                 errorFound++;
+                                inputList.RemoveAt(i);
+                                i--;
                                 break;
                             }
                         }
-                        else if (input[i][j] == closingAngle)
+                        else if (inputList[i][j] == closingAngle)
                         {
-                            if (input[i][j - 1] == openAngle)
+                            if (inputList[i][j - 1] == openAngle)
                             {
-                                input[i] = input[i].Remove(j - 1, 2);
+                                inputList[i] = inputList[i].Remove(j - 1, 2);
                                 j = 0;
                                 wasRemoved++;
                             }
-                            else if (!isClosingBracket(input[i][j - 1]))
+                            else if (!isClosingBracket(inputList[i][j - 1]))
                             {
-                                logErrors(input[i][j]);
+                                logErrors(inputList[i][j]);
                                 errorFound++;
+                                inputList.RemoveAt(i);
+                                i--;
                                 break;
                             }
                         }
@@ -120,13 +136,15 @@ namespace AoC
                     
                 }
 
-                if ( wasRemoved > 0     && 
-                    input[i].Length > 0 && 
-                    errorFound == 0)
+                if (i >= 0)
                 {
-                    i--;
+                    if (wasRemoved > 0 &&
+                        inputList[i].Length > 0 &&
+                        errorFound == 0)
+                    {
+                        i--;
+                    }
                 }
-
             }
 
             result += errors.parenErrors * lookup[0];
@@ -136,21 +154,75 @@ namespace AoC
             return result;
         }
 
-        public static int Part2(string[] input)
+        public static long Part2(string[] input)
         {
             result = 0;
+            long result64 = 0;
             int i, j;
-            
+            StringBuilder sb = new StringBuilder();
 
+            foreach (string line in inputList)
+            {
+                
+                for(i = line.Length - 1; i >= 0; i--)
+                {
+                    if (line[i] == openParen)
+                        sb.Append(closingParen);
+                    if (line[i] == openSquare)
+                        sb.Append(closingSquare);
+                    if (line[i] == openCurly)
+                        sb.Append(closingCurly);
+                    if (line[i] == openAngle)
+                        sb.Append(closingAngle);
+                }
+                string str1 = sb.ToString();
+                scores.Add(scoreCorrections(str1));
 
-            return result;
+                sb.Clear();
+            }
+
+            result64 = getMiddleScore();
+
+            return result64;
+
         }
 
-        private static int removePair(string line, int index)
+        
+
+        private static long scoreCorrections(string corrections)
         {
-            line = line.Remove(index - 1, 2);
+            long score = 0;
+            int i;
+            for (i = 0; i < corrections.Length; i++)
+            {
+                score = 5 * score;
+                if(corrections[i] == closingParen)
+                {
+                    score += 1;
+                }
+                else if (corrections[i] == closingSquare)
+                {
+                    score += 2;
+                }
+                else if (corrections[i] == closingCurly)
+                {
+                    score += 3;
+                }
+                else if (corrections[i] == closingAngle)
+                {
+                    score += 4;
+                }
+
+            }
             
-            return 1;
+            return score;
+        }
+
+        private static long getMiddleScore()
+        {
+            scores.Sort();
+            
+            return scores[scores.Count / 2];
         }
 
         private static void logErrors(char error)
