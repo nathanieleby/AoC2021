07e114791a50194a289da6ec39e1b9c63e46c467
Nathaniel Moore <nathaniel.moore@aseifl.com>
2022-01-03 10:28:16 -0600

    Solved day 11 part 2. Remeber to start at part 2 at step 100!

--- dec1/AoC1/day11.cs
+++ dec1/AoC1/day11.cs
@@ -10,8 +10,6 @@ namespace AoC
 
     class Day11
     {
-
-
         static int result = 0;
         const int steps = 100;
 
@@ -21,7 +19,6 @@ namespace AoC
         {
             result = 0;
             int i = 0, j = 0, k = 0;
-            int tempLength;
 
             //put the string array into an int list
             //initilize a grid to track if the octopus has flashed that round yet
@@ -34,15 +31,12 @@ namespace AoC
                 {
                     for (j = 0; j < octopusGrid[i].Length; j++)
                     {
-                        increaseEnergy(i, j);
+                        result += increaseEnergy(i, j);
                     }
                 }
 
-                tempLength = hasFlashed[0].Length;
-
-                //reset the hasFlashed grid
-                for (i = 0; i < hasFlashed.Count; i++)
-                    hasFlashed[i] = new bool[tempLength];
+                resetFlashGrid();
+                
             }
 
             return result;
@@ -51,49 +45,84 @@ namespace AoC
         public static long Part2(string[] input)
         {
             result = 0;
+            int i, j;
+            int flashCount = 0;
+            int numberOfSteps = 100; // start at 100 to account for the 100 steps taken in part 1
+            int gridSize = octopusGrid.Count * octopusGrid[0].Length;
             
+            while(flashCount < gridSize)
+            {
+                numberOfSteps++;
+                flashCount = 0;
+                for (i = 0; i < octopusGrid.Count; i++)
+                {
+                    for (j = 0; j < octopusGrid[i].Length; j++)
+                    {
+                        flashCount = increaseEnergy(i, j);
+
+                        if (flashCount >= gridSize)
+                            break;
+                    }
+                    if (flashCount >= gridSize)
+                        break;
+                }
+
+                resetFlashGrid();
+            }
+
+            result = numberOfSteps;
 
             return result;
 
         }
 
-        private static void increaseEnergy(int i, int j)
+        private static int increaseEnergy(int i, int j)
         {
+            int flashCounter = 0;
+            
             if (hasFlashed[i][j] == true)
-                return;
+                return 0;
 
-            int k, l;
 
             if (octopusGrid[i][j] == 9)
             {
                 octopusGrid[i][j] = 0;
                 hasFlashed[i][j] = true;
 
-                result++;
+                flashCounter++;
 
                 if(i > 0 && j > 0)
-                    increaseEnergy(i - 1, j - 1);
+                    flashCounter += increaseEnergy(i - 1, j - 1);
                 if(i > 0)
-                    increaseEnergy(i - 1, j);
+                    flashCounter += increaseEnergy(i - 1, j);
                 if(i > 0 && j < octopusGrid[i].Length - 1)
-                    increaseEnergy(i - 1, j + 1);
+                    flashCounter += increaseEnergy(i - 1, j + 1);
                 
                 if(j > 0)
-                    increaseEnergy(i, j - 1);
+                    flashCounter += increaseEnergy(i, j - 1);
                 if(j < octopusGrid[i].Length - 1)
-                    increaseEnergy(i, j + 1);
+                    flashCounter += increaseEnergy(i, j + 1);
 
                 if (i < octopusGrid.Count - 1 && j > 0)
-                    increaseEnergy(i + 1, j - 1);
+                    flashCounter += increaseEnergy(i + 1, j - 1);
                 if(i < octopusGrid.Count - 1)
-                    increaseEnergy(i + 1, j);
+                    flashCounter += increaseEnergy(i + 1, j);
                 if (i < octopusGrid.Count - 1 && j < octopusGrid[i].Length - 1)
-                    increaseEnergy(i + 1, j + 1);
+                    flashCounter += increaseEnergy(i + 1, j + 1);
             }
             else
                 octopusGrid[i][j]++;
 
-            return;
+            return flashCounter;
+        }
+
+        private static int resetFlashGrid( )
+        {
+            int i, j, tempLength = hasFlashed[0].Length;
+
+            for (i = 0; i < hasFlashed.Count; i++)
+                hasFlashed[i] = new bool[tempLength];
+            return 0;
         }
 
 
