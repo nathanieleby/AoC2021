22b41d2b763289ac6a2f18b28adc889a869e0d3f
Nathaniel Moore <nathaniel.moore@aseifl.com>
2022-01-03 09:20:42 -0600

    Solved day 11 part 1

--- dec1/AoC1/day11.cs
+++ dec1/AoC1/day11.cs
@@ -13,25 +13,37 @@ namespace AoC
 
 
         static int result = 0;
+        const int steps = 100;
 
         private static List<int[]> octopusGrid = new List<int[]>();
         private static List<bool[]> hasFlashed = new List<bool[]>();
         public static int Part1(string[] input)
         {
             result = 0;
-            int i = 0, j = 0;
+            int i = 0, j = 0, k = 0;
+            int tempLength;
 
+            //put the string array into an int list
+            //initilize a grid to track if the octopus has flashed that round yet
             parseInput(input);
 
-            for(i = 0; i< octopusGrid.Count; i++)
+            //loop through all the steps
+            for (k = 0; k < steps; k++)
             {
-                for(j = 0; j < octopusGrid[i].Length; j++)
+                for (i = 0; i < octopusGrid.Count; i++)
                 {
-                    increaseEnergy(i , j);
+                    for (j = 0; j < octopusGrid[i].Length; j++)
+                    {
+                        increaseEnergy(i, j);
+                    }
                 }
+
+                tempLength = hasFlashed[0].Length;
+
+                //reset the hasFlashed grid
+                for (i = 0; i < hasFlashed.Count; i++)
+                    hasFlashed[i] = new bool[tempLength];
             }
-            
-            
 
             return result;
         }
@@ -57,19 +69,31 @@ namespace AoC
                 octopusGrid[i][j] = 0;
                 hasFlashed[i][j] = true;
 
-                increaseEnergy(i - 1, j - 1);
-                increaseEnergy(i - 1, j);
-                increaseEnergy(i - 1, j + 1);
-                
-                increaseEnergy(i, j - 1);
-                increaseEnergy(i, j + 1);
+                result++;
 
-                increaseEnergy(i + 1, j - 1);
-                increaseEnergy(i + 1, j);
-                increaseEnergy(i + 1, j + 1);
+                if(i > 0 && j > 0)
+                    increaseEnergy(i - 1, j - 1);
+                if(i > 0)
+                    increaseEnergy(i - 1, j);
+                if(i > 0 && j < octopusGrid[i].Length - 1)
+                    increaseEnergy(i - 1, j + 1);
+                
+                if(j > 0)
+                    increaseEnergy(i, j - 1);
+                if(j < octopusGrid[i].Length - 1)
+                    increaseEnergy(i, j + 1);
+
+                if (i < octopusGrid.Count - 1 && j > 0)
+                    increaseEnergy(i + 1, j - 1);
+                if(i < octopusGrid.Count - 1)
+                    increaseEnergy(i + 1, j);
+                if (i < octopusGrid.Count - 1 && j < octopusGrid[i].Length - 1)
+                    increaseEnergy(i + 1, j + 1);
             }
             else
                 octopusGrid[i][j]++;
+
+            return;
         }
 
 
